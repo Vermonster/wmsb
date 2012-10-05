bf99b9997e766e8032081854cf1373ab1d514e8b
Steve Masterman <steve@vermonster.com>
2012-10-05 11:28:51 -0400

    rails new wmsb -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wmsb::Application.config.secret_token = 'ff13a48a6e8bb755a1841f11d45b0c482253a3779dd09e778b45a5dfb3d7ce31d7e204e5c34237bb19ee99c2dafcba0d0dad89138cb41f1786e3134b336e553c'
