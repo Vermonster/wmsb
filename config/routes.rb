1b5de573d92246056ef9c6cbd0c774d4b304c37d
Steve Masterman <steve@vermonster.com>
2012-10-05 14:49:14 -0400

    create SiteController and home page

--- config/routes.rb
+++ config/routes.rb
@@ -46,9 +46,7 @@ Wmsb::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'site#index'
 
   # See how all your routes lay out with "rake routes"
 
