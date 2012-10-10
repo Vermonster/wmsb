bf365bfb039e9a33b86cf6fbf0241bb72db478f3
Steve Masterman <steve@vermonster.com>
2012-10-10 14:49:02 -0400

    initialize buses collection

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,14 @@
 class SiteController < ApplicationController
   def index
-
+    @buses = [
+      {
+        name: 'Alice',
+        bus_no: '1'
+      },
+      {
+        name: 'Bob',
+        bus_no: '2'
+      }
+    ]
   end
 end
