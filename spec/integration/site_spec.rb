3b05bf666046b43a39bb87fc9c89e08d991fda2a
Steve Masterman <steve@vermonster.com>
2012-10-10 14:50:13 -0400

    home page spec

--- spec/integration/site_spec.rb
+++ spec/integration/site_spec.rb
@@ -4,5 +4,31 @@ feature 'Home page', js: true do
   scenario 'visit home page' do
     visit '/'
     page.should have_content "Where's My School Bus"
+    page.should have_css 'a:contains(Alice)'
+  end
+
+  scenario 'view bus map and go back' do
+    visit '/'
+    click_link 'Alice'
+
+    page.should have_css 'div.map'
+
+    within 'div.map' do
+      page.should have_content '1'
+      page.should have_no_content '2'
+    end
+
+    click_link 'Back'
+
+    page.should have_no_css 'div.map'
+
+    click_link 'Bob'
+
+    page.should have_css 'div.map'
+
+    within 'div.map' do
+      page.should have_content '2'
+      page.should have_no_content '1'
+    end
   end
 end
