da95dc4e6af3f75850682dcf9ef9827ca91c7325
Eduardo Gutierrez <eduardo@vermonster.com>
2013-04-05 16:54:42 -0400

    Setup module to talk to Zonar API

---
+++ app/models/zonar.rb
@@ -0,0 +1,36 @@
+module Zonar
+  class << self
+    private
+    attr_accessor :connection
+  end
+
+  self.connection = Faraday.new(ENV['ZONAR_API'])
+
+  def self.bus_location(bus_id)
+    params = default_params.merge(
+      action: :showposition,
+      type: :Standard,
+      logvers: 3,
+      operation: :current,
+      format: :xml,
+      reqtype: :fleet,
+      target: bus_id
+    )
+
+    response = connection.get('interface.php', params)
+
+    if response.success?
+      Hash.from_xml(response.body)
+    else
+      {}
+    end
+  end
+
+  def self.default_params
+    @default_params ||= {
+      username: ENV['ZONAR_USERNAME'],
+      password: ENV['ZONAR_PASSWORD'],
+    }
+  end
+  private_class_method :default_params
+end
