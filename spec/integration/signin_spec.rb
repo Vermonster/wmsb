f90266926f8613b5ae0126a29ace3fa210d28f07
Meg Broughton <meg@vermonster.com>
2013-04-11 16:21:16 -0400

    used simple form for login form

--- spec/integration/signin_spec.rb
+++ spec/integration/signin_spec.rb
@@ -7,9 +7,9 @@ feature 'Sessions' do
 
     visit root_path
 
-    fill_in 'family_name', with: 'Stark'
-    fill_in 'student_number', with: '1'
-    fill_in 'date_of_birth', with: '10/30/2010'
+    fill_in 'session_family_name', with: 'Stark'
+    fill_in 'session_student_number', with: '1'
+    fill_in 'session_date_of_birth', with: '10/30/2010'
     click_button 'Sign In'
 
     current_path.should eq buses_path
--- spec/support/session_steps.rb
+++ spec/support/session_steps.rb
@@ -5,9 +5,9 @@ module SessionSteps
 
     visit root_path
 
-    fill_in 'family_name', with: 'Stark'
-    fill_in 'student_number', with: '1'
-    fill_in 'date_of_birth', with: '10/30/2010'
+    fill_in 'session_family_name', with: 'Stark'
+    fill_in 'session_student_number', with: '1'
+    fill_in 'session_date_of_birth', with: '10/30/2010'
     click_button 'Sign In'
   end
 end
