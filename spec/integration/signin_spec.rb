5a80baf19197ab6e6a0c0b23b7c9d47e0273670e
Eduardo Gutierrez <eduardo@vermonster.com>
2013-04-12 17:05:33 -0400

    Use sign_in macro for base session spec

--- spec/integration/signin_spec.rb
+++ spec/integration/signin_spec.rb
@@ -2,15 +2,7 @@ require 'spec_helper'
 
 feature 'Sessions' do
   scenario 'require student number, family name and date of birth' do
-    stub_contact_id_api [200, {}, '"759393"']
-    stub_assignments_api [200, {}, [].to_json]
-
-    visit root_path
-
-    fill_in 'session_family_name', with: 'Stark'
-    fill_in 'session_student_number', with: '1'
-    fill_in 'session_date_of_birth', with: '10/30/2010'
-    click_button 'Sign In'
+    sign_in
 
     current_path.should eq buses_path
   end
