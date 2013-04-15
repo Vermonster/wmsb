f607fb991509e173a9c32fd0f4e0abe0e5e92c09
Eduardo Gutierrez <eduardo@vermonster.com>
2013-04-15 09:16:25 -0400

    Optionally enable basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  if ENV['ENABLE_HTTP_BASIC_AUTH']
+    http_basic_authenticate_with :realm => 'WMSB', :name => ENV['HTTP_BASIC_AUTH_NAME'], :password => ENV['HTTP_BASIC_AUTH_PASSWORD']
+  end
 end
