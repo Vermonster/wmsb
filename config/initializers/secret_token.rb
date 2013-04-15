56e69c6b699bc8885b9456ed4613e714dbac0803
Eduardo Gutierrez <eduardo@vermonster.com>
2013-04-15 13:30:24 -0400

    Set secret token from environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Wmsb::Application.config.secret_token = 'ff13a48a6e8bb755a1841f11d45b0c482253a3779dd09e778b45a5dfb3d7ce31d7e204e5c34237bb19ee99c2dafcba0d0dad89138cb41f1786e3134b336e553c'
+Wmsb::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'ff13a48a6e8bb755a1841f11d45b0c482253a3779dd09e778b45a5dfb3d7ce31d7e204e5c34237bb19ee99c2dafcba0d0dad89138cb41f1786e3134b336e553c'
