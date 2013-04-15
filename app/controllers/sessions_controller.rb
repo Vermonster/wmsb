a52e0a5dc6f928fbf12e1bac1e89a104a5018d5a
Meg Broughton <meg@vermonster.com>
2013-04-15 10:44:36 -0400

    update params supplied to ContactId

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    contact = ContactId.find(params[:session])
+    contact = ContactId.find(session_params)
 
     if contact.errors.any?
       redirect_to :root, alert: contact.errors.full_messages.first
@@ -15,6 +15,23 @@ class SessionsController < ApplicationController
       session[:signed_in_at]   = Time.zone.now.to_s
 
       redirect_to :buses
+    end
+  end
+
+  private
+
+  def session_params
+    @session_params ||= begin
+      hash = params[:session].dup
+
+      date_of_birth = Time.zone.local(
+        hash.delete('date_of_birth(1i)').to_i,
+        hash.delete('date_of_birth(2i)').to_i,
+        hash.delete('date_of_birth(3i)').to_i
+      )
+      hash[:date_of_birth] = date_of_birth.strftime('%m/%d/%Y')
+
+      hash
     end
   end
 end
