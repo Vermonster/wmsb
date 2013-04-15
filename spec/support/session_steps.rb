2a769b669f2d68e230e024bdd936c95dd8ab3c74
Eduardo Gutierrez <eduardo@vermonster.com>
2013-04-15 14:45:29 -0400

    Allow passing credentials used for signing in

--- spec/support/session_steps.rb
+++ spec/support/session_steps.rb
@@ -1,17 +1,23 @@
 module SessionSteps
-  def sign_in
+  def sign_in(credentials = {})
     stub_contact_id_api [200, {}, '"759393"']
     stub_assignments_api [200, {}, [].to_json]
 
-    year = 10.years.ago.year.to_s
+    credentials.reverse_merge!(
+      family_name: 'Stark',
+      student_number: '1',
+      date_of_birth: 10.years.ago
+    )
+
+    dob = credentials[:date_of_birth]
 
     visit root_path
 
-    fill_in 'session_family_name', with: 'Stark'
-    fill_in 'session_student_number', with: '1'
-    select year, from: 'session_date_of_birth_1i'
-    select 'October', from: 'session_date_of_birth_2i'
-    select '30', from: 'session_date_of_birth_3i'
+    fill_in 'session_family_name', with: credentials[:family_name]
+    fill_in 'session_student_number', with: credentials[:student_number]
+    select dob.year.to_s, from: 'session_date_of_birth_1i'
+    select dob.strftime('%B'), from: 'session_date_of_birth_2i'
+    select dob.day.to_s, from: 'session_date_of_birth_3i'
     click_button 'Sign In'
   end
 end
