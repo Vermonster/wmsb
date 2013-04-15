3b65877beab532f6a51a6625a11867de9d65ab40
Eduardo Gutierrez <eduardo@vermonster.com>
2013-04-15 11:37:49 -0400

    Fix specs from change to years in login form

--- spec/support/session_steps.rb
+++ spec/support/session_steps.rb
@@ -3,11 +3,13 @@ module SessionSteps
     stub_contact_id_api [200, {}, '"759393"']
     stub_assignments_api [200, {}, [].to_json]
 
+    year = 10.years.ago.year.to_s
+
     visit root_path
 
     fill_in 'session_family_name', with: 'Stark'
     fill_in 'session_student_number', with: '1'
-    select '2010', from: 'session_date_of_birth_1i'
+    select year, from: 'session_date_of_birth_1i'
     select 'October', from: 'session_date_of_birth_2i'
     select '30', from: 'session_date_of_birth_3i'
     click_button 'Sign In'
