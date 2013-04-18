f1b16a674c97e346458e9a646a08cfd2eacf448e
Meg Broughton <meg@vermonster.com>
2013-04-18 16:41:50 -0400

    add fontello to project for icon fonts

--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,12 @@ module Wmsb
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Add fonts path
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'font')
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
