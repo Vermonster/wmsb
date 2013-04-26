171775a802332f6cfde7134aa767900f2106a3a1
Eduardo Gutierrez <eduardo@vermonster.com>
2013-04-26 11:16:22 -0400

    Update buses spec to stub zonar history api

--- spec/integration/buses_spec.rb
+++ spec/integration/buses_spec.rb
@@ -22,23 +22,21 @@ feature 'View buses', js: true do
       )
     ]
 
-    bus_one_location = bus_location_response(
-      bus_id: '1',
-      latitude: 42.01,
-      longitude: -71.01,
-      updated_at: Time.zone.local(2010, 10, 30, 10, 30)
+    bus_one_location = bus_history_response(
+      lat: 42.01,
+      lng: -71.01,
+      time: Time.zone.local(2010, 10, 30, 10, 30)
     )
 
-    bus_two_location = bus_location_response(
-      bus_id: '2',
-      latitude: 42.02,
-      longitude: -71.02,
-      updated_at: Time.zone.local(2010, 10, 30, 10, 45)
+    bus_two_location = bus_history_response(
+      lat: 42.02,
+      lng: -71.02,
+      time: Time.zone.local(2010, 10, 30, 10, 45)
     )
 
     stub_assignments_api [200, {}, assignments.to_json]
-    stub_zonar_api [200, {}, bus_one_location]
-    stub_zonar_api [200, {}, bus_two_location]
+    stub_zonar_history_api [200, {}, [bus_one_location]]
+    stub_zonar_history_api [200, {}, [bus_two_location]]
 
     sign_in student_number: '123'
 
